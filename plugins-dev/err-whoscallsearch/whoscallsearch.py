80067e35988f71a12d6c05b7c3d4a0f7603838c2
Fawkes Wei <fawkeswei@gmail.com>
2014-03-26 17:42:59 +0800

    code clean up

--- plugins-dev/err-whoscallsearch/whoscallsearch.py
+++ plugins-dev/err-whoscallsearch/whoscallsearch.py
@@ -1,60 +1,43 @@
+# -*- coding: utf8 -*-
+
+# unused args
+# pylint: disable-msg=W0613
+# no init
+# pylint: disable-msg=W0232
+
+
 """
 
-errbot plugin to do whoscall search
+errbot plugin - WhosCallSearch
+
+search whoscall database for phone number information
 
 """
+
+from errbot import BotPlugin, botcmd
 
 import phonenumbers
 import requests
 import json
 
-from errbot import BotPlugin, botcmd
-
 class WhosCallSearch(BotPlugin):
     ''' Let's search numbers!
     '''
 
-    @botcmd                               # this tag this method as a command
-    def search(self, mess, args):          # it will respond to !hello
+    @botcmd
+    def search(self, message, args):
         ''' search info for this number
         '''
-        print self
-        print mess
         e164 = phonenumbers.format_number(phonenumbers.parse(args, 'TW'), \
                                           phonenumbers.PhoneNumberFormat.E164)
         
         yield '(whoscall) searching [' + e164 + ']...'
-
-        # return '/code ' + whoscallsearch.get_default_tag(e164)
-
-        # result = requests.get('https://api.whoscall.com/whoscallsearch/' + \
-        #                        e164.replace('+', ''),
-        #                  headers={
-        #                     "accesstoken": "k7ZI3RH2e9V14D30n4yr2td",
-        #                     "userid": "gogobot"
-        #                  })
-        # result = json.loads(result.content)
-
-        # return '/code ' + json.dumps(result, indent=4)
-
-        # return '/code ' + json.dumps(whoscallsearch.get_tag_suggestions(e164)
-        #    , indent=4)
 
         result = WhosCallSearch.get_tag_suggestions(e164)
         if len(result) <= 0:
             yield 'no info in whoscall database (sadtroll)'
         else:
             yield '/code ' + ' \n'.join(result)
-
-    @staticmethod
-    def get_default_tag(e164):
-        ''' get default tag from whoscallsearch
-        '''
-        suggestions =  WhosCallSearch.get_tag_suggestions(e164)
-        if len(suggestions) > 0:
-            return suggestions[0]
-        else:
-            return ''
 
     @staticmethod
     def get_tag_suggestions(e164):
