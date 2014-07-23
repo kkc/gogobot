18e8c71986aa831fb4db62e4ab06ae045708ce0a
NickJIan <nickjian@gogolook.com>
2014-07-23 15:09:02 +0800

    move actions to google doc

---
+++ copy_strings.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import gspread
+
+
+ColumnCount = 5
+
+
+def zhprint(obj):
+    import re
+    print re.sub(r"\\u([a-f0-9]{4})", lambda mg: unichr(int(mg.group(1), 16)), obj.__repr__())
+
+
+def refreshData(dataList, initColumn):
+    gc = gspread.login('gogobot5566', 'whoscall123')
+    if initColumn ==0:
+        wks = gc.open('gogobotThinkBrian').get_worksheet(1)
+    else:
+        wks = gc.open('gogobotThinkBrian').get_worksheet(0)
+    rowCount = wks.row_count
+
+    
+    print ' '
+    if initColumn ==1:
+        print 'start loading from spreadSheet contributed'
+    elif initColumn ==0:
+        print 'start loading from spreadSheet protected'
+
+    
+    
+
+    data = wks.get_all_values()
+    try:
+        for r in range(1,rowCount):
+
+            print '*** getting row:',r,' ***'
+
+            newData ={}
+            newData['keyword']=[]
+            temp = data[r][initColumn]
+            tempArray = temp.splitlines()
+            for keyword in tempArray:
+                if len(keyword)>0:
+                    newData['keyword'].append(keyword)
+
+            newData['response']=[]
+            temp =data[r][initColumn+1]
+            tempArray =temp.splitlines()
+            for res in tempArray:
+                newData['response'].append(res)
+
+            newData['chance'] = data[r][initColumn+2]
+
+            if('TRUE' in data[r][initColumn+3]):
+                newData['commonDia']=True
+            else :
+                newData['commonDia']=False
+
+            zhprint (newData)
+
+            if len(newData['response'])>0 and len(newData['keyword'])>0 and newData['chance']>0:
+                dataList.append(newData)
+                print 'data received'
+            else:
+                print '*** invalid data, dropped ***'
+
+    except:
+        print 'end of rows'
+
+
+def getAction():
+
+    dataList =[]
+
+    # load contributed data first
+    refreshData(dataList,1)
+    refreshData(dataList,0)
+
+    return dataList
+
+
+
+
--- plugins-dev/err-chitchat/chitchat.py
+++ plugins-dev/err-chitchat/chitchat.py
@@ -21,7 +21,11 @@ import random
 import sys
 import time
 import feedparser
+import gspread
+import copy_strings
 import xml.etree.ElementTree as xml
+from copy_strings import getAction
+
 
 
 reload(sys)
@@ -32,6 +36,9 @@ ShowCompareLog = True
 ShowEmoLog = False
 ShowTalkHis = True
 MaxHistory = 7
+LastUpdateTime = 0
+
+
 
 
 emoji = [
@@ -42,6 +49,14 @@ emoji = [
 happyString = ['爽', '好笑', '笑死']
 sadString = ['受傷', '難過', '生病', '請假', 'Q_Q', 'QQ']
 angryString = ['gogobot*爛']
+
+def zhprint(obj):
+    import re
+    print re.sub(r"\\u([a-f0-9]{4})", lambda mg: unichr(int(mg.group(1), 16)), obj.__repr__())
+
+
+
+
 
 
 class ChitChat(BotPlugin):
@@ -65,9 +80,28 @@ class ChitChat(BotPlugin):
     lastCheckTime = 0
     lastLunchTime = 0
     lastDinnerTime = 0
+    LastUpdateTime = 0
+
+    action_list = []
 
     hatedPeopleName = ''
     likePeopleName = ''
+
+    def checkUpdateKeyword(self):
+        currentTime = int(time.time())
+
+        print currentTime
+        print self.LastUpdateTime
+
+
+        if  currentTime - self.LastUpdateTime >3600:
+            print 'last update > 3600, re-fetch from spreadsheet'
+            self.action_list = getAction()
+            print 'action updated, total ',len(self.action_list) , 'action now'
+            self.LastUpdateTime =currentTime
+
+
+
 
     def saveHist(self, message, Msgfrom):
         if len(self.histMsg) < MaxHistory:
@@ -76,7 +110,7 @@ class ChitChat(BotPlugin):
         else:
             self.histMsg[self.histPisiton] = message
             self.histFrom[self.histPisiton] = Msgfrom
-            print 'memory postion: ', self.histPisiton
+            print 'memory position: ', self.histPisiton
             if self .histPisiton == MaxHistory - 1:
                 self.histPisiton = 0
             else:
@@ -111,11 +145,11 @@ class ChitChat(BotPlugin):
         if random.randrange(0, 11) < 2:
             return False
         else:
-            print 'check weather success'
 
             if self.checkIfContain([u'天氣', u'氣溫'], message_string):
                 otherDes = [u'早上', u'晚上', u'飯', u'上班', u'餐', u'下班', u'出門']
 
+                print 'check weather roll success, keyword matched'
                 if self.prevContain(otherDes) or self.checkIfContain(otherDes, message_string):
                     self.showWeahterForcast()
                 else:
@@ -187,16 +221,16 @@ class ChitChat(BotPlugin):
         # in order to increase chitchat variety, bot will sometimes go off the
         # rail and do something unexpected. sendRandomMessage() insure bot will
         # reply something meaningless occasionally.
-    def sendRandomMessage(self, action_list):
+    def sendRandomMessage(self):
         if random.randrange(0, 101) > 98:
             print ' **send random message'
 
             if random .randrange(0, 101) < 65:
 
-                action = random.choice(action_list)
+                action = random.choice(self.action_list)
                 while 'commonDia' in action and action['commonDia'] == False:
                     print ' ***not common dialog, re-roll'
-                    action = random.choice(action_list)
+                    action = random.choice(self.action_list)
 
                 totalMessage = random.choice(action['response'])
                 response_messages = totalMessage.split('*')
@@ -237,15 +271,12 @@ class ChitChat(BotPlugin):
         if len(self.histFrom) < 6:
             return False
 
-        print 'at 1'
         samePerson = True
         firstPesron = self.histFrom[0]
         for name in self.histFrom:
             if firstPesron != name:
                 samePerson = False
                 break
-
-        print 'at 1'
 
         if samePerson:
             if random.randrange(0, 101) > 75:
@@ -342,6 +373,10 @@ class ChitChat(BotPlugin):
                 print 'angry str matched: ' + message_string
             self.angry += 1
 
+
+
+
+
     @botcmd
     def look(self, message, args):
         ''' send gogolook emoji when received command look
@@ -361,12 +396,15 @@ class ChitChat(BotPlugin):
         message_from = message.getFrom().getResource()
         self.mMessage = message
 
+        self.checkUpdateKeyword()
+
         if message_from == 'Gogo Bot':
             return
 
         self.saveHist(message_string, message_from)
 
-        action_list = []
+
+
 
         # action can now support multiple keyword and multi-line message
         #
@@ -380,364 +418,23 @@ class ChitChat(BotPlugin):
         #     ], 'chance': chance_int (%)
         # })
 
-        action_list.append({
-            'keyword': [u'gogobot*誰寫的', u'gogobot*白爛', u'gogobot*怎麼'],
-            'response': [
-                'http://i.imgur.com/aopZwmB.jpg',
-                '我記得Duo好像有說過要寫Nick什麼的*詳細情形要問他...',
-                '想也知道是@Fawkes Wei寫的啊...*Meetup不是有報告過?',
-                '我查一下...*等等.....*code裡面有@Jett的看正妹程式碼...',
-                'http://i.imgur.com/HCG9CBi.jpg', 'http://i.imgur.com/56NCcYb.jpg',
-                'http://i.imgur.com/lvig5X0.jpg',
-                'Trying to anwser:FATAL ERROR* * *Unkwon Error Occored * * * * *Abort, Retry, Fail?',
-                '你什麼時候有我是bot的錯覺'
-
-            ], 'chance': 60, 'commonDia': False
-
-        })
-
-        action_list.append({
-            'keyword': [u'gogobot*開門'],
-            'response': [
-                '自己去開'
-            ], 'chance': 70, 'commonDia': False
-
-
-        })
-
-        action_list.append({
-            'keyword': ['kakashi', u'卡西'],
-            'response': [
-                'https://dl.dropboxusercontent.com/u/3256092/hipchat/k.jpg',
-                'https://dl.dropboxusercontent.com/u/3256092/hipchat/ka2.jpg',
-                'http://i.imgur.com/4uyjCm0s.jpg',
-                'http://i.imgur.com/UZP1lll.jpg',
-                'http://i.imgur.com/leRdtZ8.jpg',
-                'http://i.imgur.com/ZHTsMsv.jpg',
-                'http://i.imgur.com/QF5T4kF.jpg',
-                'http://i.imgur.com/445rcyh.png',
-
-                'https://dl.dropboxusercontent.com/u/3256092/hipchat/k.jpg',
-                'https://dl.dropboxusercontent.com/u/3256092/hipchat/ka2.jpg',
-                'http://i.imgur.com/4uyjCm0s.jpg',
-                'http://i.imgur.com/UZP1lll.jpg',
-                'http://i.imgur.com/leRdtZ8.jpg',
-                'http://i.imgur.com/ZHTsMsv.jpg',
-                'http://i.imgur.com/QF5T4kF.jpg',
-                'http://i.imgur.com/445rcyh.png',
-
-
-                'http://i.imgur.com/DR626eA.jpg'
-            ], 'chance': 60, 'commonDia': True
-
-
-        })
-
-        action_list.append({
-            'keyword': [u'長老', 'TheonLin'],
-            'response': [
-                'http://i.imgur.com/aopZwmB.jpg',
-                'http://i.imgur.com/DRh82Z9.jpg',
-                'http://i.imgur.com/Z08bRz7.jpg',
-                'http://i.imgur.com/1zS3S1k.jpg',
-                'http://i.imgur.com/3exOy6F.png',
-                'http://i.imgur.com/A5QZZeb.jpg',
-                'http://i.imgur.com/pRZewh2.jpg',
-                'http://i.imgur.com/Nr3FQwL.jpg',
-                'http://i.imgur.com/csImqM4.jpg',
-                'http://i.imgur.com/XL8drcy.jpg',
-                'http://i.imgur.com/cuC462y.jpg',
-                'http://pic2.52pk.com/files/121218/1809687_173515_1.jpg'
-            ], 'chance': 60, 'commonDia': True
-
-
-        })
-
-        # action_list.append({
-        #     'keyword': [u'回話', u'說話'],
-        #     'response': [
-        #         json.loads(
-        #             requests.get('http://more.handlino.com/sentences.json?'
-        #                          'limit=27').content
-        #         )['sentences'][0],
-        #     ], 'chance': 90
-        # })
-
-        action_list.append({
-            'keyword': [u'喝什麼', '買飲料', '喝飲料'],
-            'response': [
-                '有人要請客嗎？', '請我我就喝', '星八克',
-                '50嵐', '50嵐', '50嵐', '50嵐',
-                '英國藍', '英國藍',
-                '水巷茶弄',
-                'coco都可', 'coco都可', 'coco都可',
-                '橘子工坊', '橘子工坊', '橘子工坊',
-                '喝什麼! 喝水啊!!', '喝什麼! 喝水啊!!',
-                '喝什麼! 喝水啊!!'
-            ], 'chance': 70, 'commonDia': True
-
-        })
-
-        action_list.append({
-            'keyword': [u'安安', u'你好', 'hihi'],
-            'response': [
-                '安安 幾歲 住哪',
-                '衝山小啦',
-                '來來來來來 哩來',
-                '(troll)',
-                '你好',
-                'http://i.imgur.com/5VwOCnq.png',
-                'http://i.imgur.com/mAuzhW9.jpg'
-            ], 'chance': 60, 'commonDia': True
-
-        })
-
-        action_list.append({
-            'keyword': [u'gogobot*機器人', 'gogobot*真人', '你*真人', '你*機器人', '你*電腦', '你*程式'],
-            'response': [
-            '我才不是機器人*機器人會這樣和你講話嗎？',
-            '難道我每天都上線也要和你說？',
-            '我是機器人？ *'
-            '我好像聽到什麼不得了的事情？'
-            '不要為了要我回文亂發言好嗎？', '請勿拍打餵食', '你說呢？',
-            'http://i.imgur.com/ZujjkBz.jpg',
-            '你什麼時候有我是bot的錯覺'
-
-            ], 'chance': 70, 'commonDia': False
-
-        })
-
-        action_list.append({
-            'keyword': ['duo', u'丟神'],
-            'response': [
-                'http://i.imgur.com/MgVvp9E.jpg',
-                'http://i.imgur.com/MgVvp9E.jpg',
-                'http://i.imgur.com/Zt23LI9.png',
-                'http://i.imgur.com/ZujjkBz.jpg'
-
-
-            ], 'chance': 30, 'commonDia': True
-
-        })
-
-        action_list.append({
-            'keyword': ['jeff'],
-            'response': [
-                'http://i.imgur.com/5WhQW6S.jpg',
-                'http://i.imgur.com/aJZHFAN.jpg'
-                'http://magimg.chinayes.com/Mags/M73/20140708/Article/Content/201407081152491244288.jpg'
-
-            ], 'chance': 20, 'commonDia': False
-        })
-
-        action_list.append({
-            'keyword': ['生病*請假', '受傷*請假', '不舒服*請假'],
-            'response': [
-                'http://i.imgur.com/TRdgTCp.jpg'
-            ], 'chance': 55, 'commonDia': True
-
-        })
-
-        action_list.append({
-            'keyword': ['好慘', '倒楣', '超雖', '有夠雖'],
-            'response': [
-                'http://i.imgur.com/TRdgTCp.jpg', 'http://i.imgur.com/tizCXON.jpg', 'http://i.imgur.com/Otxbjoz.jpg'
-            ], 'chance': 55, 'commonDia': True
-        })
-
-        action_list.append({
-            'keyword': ['為什麼', '怎麼', '如何'],
-            'response': [
-                'http://i.imgur.com/56NCcYb.jpg', 'http://i.imgur.com/ZujjkBz.jpg'
-            ], 'chance': 55, 'commonDia': True
-        })
-
-        action_list.append({
-            'keyword': [u'吃飯'],
-            'response': [
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*不能吃太胖喔，會被殺掉的',
-
-                'http://static.nownews.com/newspic/1668/i1668882.jpg*多吃一點喔，等下把你殺掉！'
-
-            ], 'chance': 60, 'commonDia': True
-        })
-
-        action_list.append({
-            'keyword': ['gogobot', '@gogobot', 'Gogo*Bot', 'gogo*Bot'],
-            'response': [
-                u'有人找我嗎 (fuckyeah)', '誰理你',
-                u'有人找我嗎 (fuckyeah)', '誰理你',
-                '來者何人？', '你好 A_A', '關我屁事？', '先走了掰～', '怪我摟',
-                '來者何人？', '你好 A_A', '關我屁事？', '先走了掰～', '怪我摟',
-                ' (fuckyeah)',
-                '(dumb)',
-                '(pokerface)',
-                '(areyoukiddingme)',
-                '(lol)',
-                '(troll)',
-                '(wat)',
-                '(wtf)',
-                '(content)',
-                '(ohcrap)',
-                '(derp)',
-                '(dumb)',
-                '(poo)',
-                'http://i.imgur.com/mAuzhW9.jpg', 'http://i.imgur.com/mAuzhW9.jpg',
-                'http://i.imgur.com/5s46UnG.jpg', 'http://i.imgur.com/5s46UnG.jpg',
-                'http://i.imgur.com/8J0DPac.jpg', 'http://i.imgur.com/8J0DPac.jpg'
-
-            ], 'chance': 75, 'commonDia': True
-        })
-
-        action_list.append({
-            'keyword': ['(thumbsup)'],
-            'response': [
-                'https://dl.dropboxusercontent.com/u/3256092/hipchat/like.png',
-                'http://i.imgur.com/mAuzhW9.jpg',
-                '(thumbsup)', '(thumbsup)', '(thumbsup)'
-            ], 'chance': 40, 'commonDia': True
-        })
-
-        action_list.append({
-            'keyword': ['nick'],
-            'response': [
-                'http://i.imgur.com/5VwOCnq.png'
-            ], 'chance': 10, 'commonDia': False
-        })
-
-        action_list.append({
-            'keyword': ['gogolook'],
-            'response': [
-                '(gogolook)',
-
-            ], 'chance': 70, 'commonDia': True
-        })
-
-        action_list.append({
-            'keyword': [u'講笑話', u'聽笑話'],
-            'response': [
-                '有两个程序员钓鱼，其中一个钓到一条美人鱼，*这个美人鱼上半身是美女，下半身是鱼，*于是这个程序员就吧她放了，*另一个问他：“Why？” *他回答说：“没有API。”',
-                '"爸爸，為什麼大象的鼻子這麼長呢？"*"那是規格啊。"',
-                '女孩："我警告你，你再不停手我就要叫警察了喔。"*程式設計師："警告不重要啦，不是錯誤就好。"',
-                '"為什麼程式設計師常常不會分萬聖節跟聖誕節？"*"喔，因為OCT 31 = DEC 25啊。"',
-                '不懂電腦的操作者發現Bug的頻率經常比QA工程師還高，*缺點是他們通常沒有辦法重現這個Bug。',
-                '使用手冊是需要的人不會讀它，而不需要的人會去讀的神祕讀物。',
-                '沒被發現的 bug，就不是 bug！',
-                '程序員最討厭康熙的哪個兒子？ *答：胤禩。 *因為他是八阿哥。',
-                'An SQL statement enters a bar, walks up to two tables and says *“can I join you?”'
-            ], 'chance': 70, 'commonDia': False
-        })
-
-        action_list.append({
-            'keyword': [u'回家', u'早退', u'打混'],
-            'response': [
-                'http://i.imgur.com/jPO69zy.jpg', 'http://i.imgur.com/lvig5X0.jpg'
-            ], 'chance': 50, 'commonDia': True
-        })
-
-        action_list.append({
-            'keyword': [u'對不對'],
-            'response': [
-                '對', '對啊', '不對', '不太對', "好像是", '不對啊', '我不是搞諜報出身，不好評論', '你問看看' +
-            random.choice(self.histFrom) + ' 他比較清楚'
-            ], 'chance': 50, 'commonDia': False
-        })
-
-        action_list.append({
-            'keyword': [u'是不是'],
-            'response': [
-                '是', '是啊', '不是', '不是啊', '我不是搞諜報出身，不好評論', '你問看看' +
-            random.choice(self.histFrom) + ' 他比較清楚'
-            ], 'chance': 50, 'commonDia': False
-        })
-
-        action_list.append({
-            'keyword': [u'gogo*道歉'],
-            'response': [
-                '對不起', '對不住啊', '對不起', '對不住啊', '對不起..  等等 我是不是應該露出什麼東西？'
-            ], 'chance': 70, 'commonDia': True
-        })
-
-        action_list.append({
-            'keyword': [u'QQ', 'Q_Q', 'sad'],
-            'response': [
-                '對不起', 'QQ', 'sad', 'Q_Q'
-            ], 'chance': 60, 'commonDia': True
-        })
-
-        action_list.append({
-            'keyword': [u'天安門事件'],
-            'response': [
-                'Gogo Bot left the room (user disconnected)'
-            ], 'chance': 70, 'commonDia': True
-        })
-
-        action_list.append({
-                           'keyword': [u'強'],
-                           'response': [
-                               '強', '強！'
-                           ], 'chance': 70, 'commonDia': True
-                           })
-
-        action_list.append({
-                           'keyword': [u'sorry','我錯了'],
-                           'response': [
-                               '下次不要再犯', '知道錯就好了'
-                           ], 'chance': 60, 'commonDia': False
-                           })
-
-        action_list.append({
-                           'keyword': [u'XDD','太好笑'],
-                           'response': [
-                               '太好笑了', 'XDDDD','XD'
-                           ], 'chance': 30, 'commonDia': False
-                           })
-
-        action_list.append({
-                           'keyword': ['＝ ='],
-                           'response': [
-                               '= =|', 'XDDDD','XD','= ='
-                           ], 'chance': 30, 'commonDia': False
-                           })
-
-        print '1'
-
         # 亂回機制1
-        if self.sendRandomMessage(action_list):
+        if self.sendRandomMessage():
             return
-
-        print '2'
 
         if self.checkBadPeople(message_string):
             return
-
-        print '3'
 
         if 'Duo Ho' in message_from and random.randrange(0, 101) < 10:
             self.send(message.getFrom(),
                       random.choice(['強', '強！', '丟神！', '霸氣']),
                       message_type=message.getType())
-        print '4'
 
         if self.checkWeather(message_string):
             return
 
-        print '5'
-
         if self.antiWash():
             return
-
-        print '6'
 
         if ShowEmoLog:
             print 'emotion before update'
@@ -747,19 +444,17 @@ class ChitChat(BotPlugin):
             print 'emotion after update'
             self.printCurrentEmotion()
 
-        print '7'
-
         if ShowCompareLog:
             print '*** message received, try to responese ***'
             print 'message: ', message_string
 
         # do not trigger on links
         if 'http:' in message_string or 'https:' in message_string:
-            self.sendRandomMessage(action_list)
+            self.sendRandomMessage(self.action_list)
             return
 
         # start to search if it match any keyword in action_list
-        for action in action_list:
+        for action in self.action_list:
             if ShowCompareLog:
                 print 'see if matched keyword type: ', action['keyword'][0]
 
@@ -769,7 +464,7 @@ class ChitChat(BotPlugin):
 
                 if 'chance' in action:
                     if ShowCompareLog:
-                        print ' ** ', action['keyword'], 'has "roll" key, have to roll '
+                        zhprint( ' ** '+ action['keyword'] +'has "roll" key, have to roll ')
                     if random .randrange(0, 101) < action['chance']:
                         if ShowCompareLog:
                             print ' **rand < ', action['chance'], ', roll success!!'
@@ -795,10 +490,11 @@ class ChitChat(BotPlugin):
                                   msg,
                                   message_type=message.getType())
                         if ShowCompareLog:
-                            print 'message "', response_messages, '" sended'
+                            zhprint( 'message "'+ response_messages+ '" sended')
 
                     return
 
         # 亂回機制2
-        if self.sendRandomMessage(action_list):
+        if self.sendRandomMessage():
             return
+
